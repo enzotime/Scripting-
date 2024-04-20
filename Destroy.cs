7643aeffebcb48694b50ef77d7ef734f6a539b99
enzotime <89253204+enzotime@users.noreply.github.com>
2024-04-20 15:02:14 +0800

    Create Destroy.cs

---
+++ Destroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // output on the console of unity
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject);
+    }
+}
